6c56f6a92740940541ec1f1b4c9e8364f6a77c0f
lcbordelon <71234155+lcbordelon@users.noreply.github.com>
2021-01-12 19:15:33 -0700

    initial server setup. connection working (#3)

---
+++ routes/api-routes.js
@@ -0,0 +1,8 @@
+// Requiring our models
+// const db = require("../models");
+
+// Routes
+module.exports = (app) => {
+  // GET route for getting all of the todos
+  console.log("display this shit");
+};
