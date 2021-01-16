f285a650242215a4567bb5275b297a954fb2cd50
Robby Ray <70773240+rgr5035@users.noreply.github.com>
2021-01-16 12:01:18 -0700

    worked on lists.handlebars (#10)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@ const db = require("../models");
 
 // Routes
 module.exports = (app) => {
-  // GET route for getting all of the todos
+  // GET route for getting all of the list members
   console.log("display this shit");
   app.get('/api/lists', (req, res) => {
     db.ListMember.findAll({}).then((dbListMember) => res.json(dbListMember));
