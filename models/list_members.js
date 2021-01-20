c77fc51d41e20d16e8f9f239650692e6e44fca0f
Robby Ray <70773240+rgr5035@users.noreply.github.com>
2021-01-19 22:11:20 -0700

    New branch (#31)
    * got list items to display on lists.handlebars
    
    * added styling to lists.handlebars
    
    * worked on list styling
    
    * worked on list joins
    
    * TRYINGgit status!
    
    * trying to debug associations
    
    Co-authored-by: Lily'okalani Hafoka <68487859+LHafoka13@users.noreply.github.com>

--- models/list_members.js
+++ models/list_members.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
     const ListMember = sequelize.define('ListMember', {
       name: DataTypes.STRING,
+      // giftItems: DataTypes.JSON,
     });
 
     // ListMember.associate = (models) => {
--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -7,22 +7,39 @@ const db = require("../models");
 
     const query = {};
 
+
+    if (req.query.id) {
+      query.ListMemberId = req.query.id;
+    }
+    
+    db.GiftItem.findAll({
+      // where: query,
+
     if (req.query.listmember_id) {
       query.listmemberId = req.query.listmember_id;
     }
     
     db.GiftItem.findAll({
       where: query,
+
       include: [db.ListMember],
     }).then((dbGiftItem) => 
     console.log(dbGiftItem));
   });
 
 
+
+  //nothing
+
+
+
   // api call to post (create) one record in the gift_items db
   app.post("/api/items", (req, res) => {
     db.GiftItem.create(req.body).then((dbGiftItem) => res.json(dbGiftItem));
   });
+
+
+  // app.put
 
   // api call to delete (delete) one record in the gift_items db
   // app.delete("/api/items/:id", (req, res) => {
@@ -31,6 +48,16 @@ const db = require("../models");
   //       id: req.params.id,
   //     },
   //   }).then((dbGiftItem) => res.json(dbGiftItem));
+
+
+  // api call to delete (delete) one record in the gift_items db
+  // app.delete("/api/items/:id", (req, res) => {
+  //   db.GiftItem.destroy({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   }).then((dbGiftItem) => res.json(dbGiftItem));
+
 
   // });
 };
