9bb27bb7f40cc0fc52aafb12e8f4347f7d47b179
Robby Ray <rgr5035@gmail.com>
2021-01-21 20:58:03 -0700

    updated files

--- models/gift_items.js
+++ models/gift_items.js
@@ -1,17 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const GiftItem = sequelize.define("GiftItem", {
-    item: DataTypes.STRING,
-    desc: DataTypes.STRING,
+    item: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    desc: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+        len: [1],
+    },
+    
   });
 
-  // // Add a belongsTo association to lists here
-  // GiftItem.associate = function (models) {
-  //   GiftItem.belongsTo(models.list_members, {
-  //     foreignKey: {
-  //       allowNull: false, 
-  //     },
-  //   });
-  // };
+  GiftItem.associate = (models) => {
+    GiftItem.belongsTo(models.ListMember, {
+      foreignKey: {
+        allowNull: false, 
+      },
+    });
+  };
 
   return GiftItem;
 };
